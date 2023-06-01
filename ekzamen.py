fe9a61c7e9bff089109b09ee367cfbc28e8c44bc
student <bukindimon444@gmail.com>
2023-06-01 19:18:49 +0300

    3

--- ekzamen.py
+++ ekzamen.py
@@ -1,6 +1,7 @@
-#2
-a = int(input("введіть число"))
-b = int(input("введіть число"))
-for i in range(a,b):
-    sum = i
-print(sum)
+#3
+s = 0
+for i in range(1,100):
+    if i % 2 == 0:
+        s += i
+print(s)
+
