aceb0e1c046646a2793f4a85d67fd20455118336
student <bukindimon444@gmail.com>
2023-06-01 20:14:10 +0300

    8

--- ekzamen.py
+++ ekzamen.py
@@ -1,8 +1,9 @@
-#7
-def get_top_student(grades_dict):
-    highest_grade = max(grades_dict.values())
-    top_student = [name for name, grade in grades_dict.items() if grade == highest_grade]
-    return top_student[0]
-grades = {'Іван': 78,'Марія': 85,'Петро': 27,'Костя': 89,'Андрій': 96}
-top_student_name = get_top_student(grades)
-print("Студент з найвищою оцінкою:", top_student_name)
+def strings(strings):
+    b = []
+    for string in strings:
+        if string[0].isupper():
+            b.append(string)
+    return b
+a = input("Введіть список рядків, розділениx прробідами: ").split(" ")
+result = strings(a)
+print("Результат:", result)
